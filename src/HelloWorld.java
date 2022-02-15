6d183a7bea75b83ec2dd49929d5be2b8f6c4ed6e
Danielle Turner <danielle.7.turner@gmail.com>
2022-02-15 16:00:35 -0600

    java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,12 @@
-public class HelloWorld {
-}
+/** Introduction to Java**/
+
+
+public class HelloWorld {                         // here it defines a public class of HelloWorld that matches the name of the java file
+    public static void main(String[] args){       // here main is the name of the method which is the entry point for our code when running java
+                                                  // passing in a name of String[] is the type of things being passed in called args-- args is going to be type of string
+                                                  // the void word signifies that it's not expecting  something to return
+        System.out.println("Hello, Danny!");      // here we print out to the console using the -System.out.println- followed by whatever is being printed to console in () surronded by qutations
+    }                                             // when using the println after the args(the text inside the paranthesis) it adds an extra line space after the output text
+}                                                 // when using the print it doesn't add an extra line space after the output text.
+
+
---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,125 @@
+/*-----Syntax, Types ona Variables---
+
+Two main categories of data types: primitive and reference
+primitive---- these data types are not objects. There only job is to store values of a specific type of [byte, short, int, long, float, double, char, boolean (see char in curriculum)]
+----[byte all the way through doubles are dealing with only numbers]
+----[char is a single character] 'would be declared with a single quote'
+----[boolean true or false]
+reference----
+**/
+
+
+public class SyntaxTypesVariables {
+
+    public static void main(String[] args){
+        int myFavoriteNumber = 7; //1
+        System.out.println(myFavoriteNumber);
+
+//      String myString = "Hey"; //2
+//      System.out.println(myString);
+
+//      String myString = "D";  //3
+//      System.out.println(myString);
+
+        String myString = "3.14159";  //4
+        System.out.println(myString);
+
+//      long myNumber; //5
+//      System.out.println(myNumber); // Here the myNumber has no value so it shows red and can't be consoled.
+
+//       long myNumber = 3.14;  //6
+//       System.out.println(myNumber);
+
+//        long myNumber = 123L; // 7
+//        System.out.println(myNumber); // prints out only the number 123 even thought there is an L
+
+        long myNumber = 123; // 8
+        System.out.println(myNumber);
+
+
+
+
+
+
+//       int myFavoriteNumber = 18;             // in order to declare a variable you must declare it's type first
+//                                             // this way is also used int myFavoriteNumber; next line below myFavoriteNumber = 18;
+//        System.out.println(myFavoriteNumber);
+//
+//        // storing a favorite letter
+//
+//        char myFavoriteLetter = 'D';
+//        System.out.println(myFavoriteLetter);
+//
+//        // boolean
+//
+//        boolean isSunny = true;  // boolean values have to be written lowercase
+//        System.out.println(isSunny);
+
+        // good to know
+        // int --> primitive type / not an object
+        // Integer --> object / give us access to manipulate the Integer as an object
+
+        // double --> primitive
+        // Double --> Object / give us access to manipulate the Integer as an object
+
+        // char --> primitive
+        // Character --> Object / give us access to manipulate the Integer as an object
+
+        // Strings --> in java are not primitive
+        // Example
+//        String school;
+//        school = "CodeUp";           //now assign a value or initalize the variable --> now CodeUp becomes a identifier after it has a value
+//        System.out.println(school);
+
+        // escaping characters
+        // ---->  "\"" // => "
+        // --->  "\\" // => \
+        // --->  "\n" // => the newline character
+        // --->  "\t" // => the tab character
+
+        /*Reserve Words*/ //---> Words that can't be used
+
+        /*abstract continue for new switch assert default goto package synchronized boolean do if private this break double implements protected throw byte else import public throws case enum instanceof
+        return transient catch extends int short try char final interface static void class finally long strictfp volatile const float native super while*/
+
+        /*Arithmetic Expressions*/
+        // Store the number 3 and 9
+
+//        int firstNumber = 3;
+//        firstNumber++; // increments number by one
+//        int secondNumber = 9;
+//        secondNumber--; // decrements number by one
+//        int result = firstNumber + secondNumber;
+//        System.out.println(result);
+//
+//        byte firstNum = 125;
+//        byte secondNum = 120;
+//        int total = firstNum % secondNum;
+//        System.out.println(total);
+
+        // Constants
+        // final keyword ----> can not re-assign the variable to anything else
+
+        // Casting --->  is converting a value of one data-type into another data-type
+        // a byte can be cast to a short but a short cannot be cast into a byte
+        // byte	    1	Very short integers from -128 to 127
+        // short	2	Short integers from -32,768 to 32,767
+
+//         // example
+//        int myInteger = 900;
+//        long morePrecise = myInteger;
+//
+//        System.out.println(myInteger);
+//        System.out.println(morePrecise);
+//
+//        //example 2
+//        double pi = 3.14159;
+//        int almostPi = (int) pi;
+//
+//        System.out.println(pi);
+//        System.out.println(almostPi);
+
+
+
+    }
+}

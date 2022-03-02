d42d4d1ab9f7377814c5df45d7043e0c418324c1
Danielle Turner <danielle.7.turner@gmail.com>
2022-03-01 22:58:03 -0600

    inclass asignment finshed

--- src/InClassAssignments/ServerNameGenerator.java
+++ src/InClassAssignments/ServerNameGenerator.java
@@ -1,25 +1,32 @@
 package InClassAssignments;
 
+import java.util.Arrays;
+
 public class ServerNameGenerator {
 
-    String[] adjective = {"red", "yellow", "pink", "small", "brown", "green", "yellow", "purple", "blue", "grey"};
-    String[] noun ={"John", "James", "Bob", "Henry", "Moses", "Paul", "Peter", "James", "Cyrus", "Ezekiel"};
+    static String[] adjective = {"red", "yellow", "pink", "small", "brown", "green", "yellow", "purple", "blue", "grey"};
+    static String[] noun ={"John", "James", "Bob", "Henry", "Moses", "Paul", "Peter", "James", "Cyrus", "Ezekiel"};
+
+    static String randomNounAdj(String[] array){
+
+        int min = 0;
+        int max = array.length;
+        int random = (int)(Math.random()* (max - min + 1));
+        String newName = "";
+
+        for(int i = 0; i < 1; i++){
+            newName = array[random];
+        }
+        return newName;
+    }
 
     public static void main(String[] args) {
+        String ranadj = randomNounAdj(adjective);
+        String randn = randomNounAdj(noun);
 
-
+        System.out.printf("Here is your server name: \n%s-%s", randn, ranadj);
 
     }
-
-    public String[] randomAdjective(String name){
-        return adjective;
-    }
-
-
-    public String[] randomNoun(String description){
-        return noun;
-    }
-
 }
 
 
